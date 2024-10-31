c0bffd3fd30433fd309ac085a03fdcb41223dc15
Kevin <antonandrade259@gmail.com>
2024-10-31 11:15:25 -0600

    Respaldo automático 2024-10-31 11:15:25

--- SmileyFaces.py
+++ SmileyFaces.py
@@ -1,9 +1,17 @@
 def countSmileys(arr):
-    faces=arr.split()
-    for face in faces:
-        if len(face)>2:
-        # if (face[0]==";") | (face[0]==":"):
+    finalArray = []
+    for face in arr:
+        if face[0] == ":" or face[0] == ";":
+            # Verifica si el segundo carácter es una nariz o una sonrisa directa
+            if len(face) == 2 and (face[1] == ")" or face[1] == "D"):
+                finalArray.append(face)
+            elif len(face) == 3 and (face[1] == "-" or face[1] == "~") and (face[2] == ")" or face[2] == "D"):
+                finalArray.append(face)
 
+    if len(finalArray) > 0:
+        return "Hay " + str(len(finalArray)) + " smileys: " + str(finalArray)
+    else:
+        return "No hay Smileys :c"
 
 print(countSmileys([":)", ";(", ";}", ":-D"]))
 print(countSmileys([";D", ":-(", ":-)", ";~)"]))
