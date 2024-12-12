6c7f8e0bedb83cfcd54b3947a144b69405fea6c9
Six66 <kevinandrade259@gmail.com>
2024-12-12 00:44:36 -0600

    Nuevo ejercicio (Expresiones Regulares Comentadas)

--- PhoneNumber.py
+++ PhoneNumber.py
@@ -2,6 +2,10 @@
 
 import re
 def isValidPhoneNumber(phone):
+    #Cada diagonal invertida significa "literal"
+    #El pattern se descompone en "literal (", "literal digito entero de 3 caracteres", "literal ) y un espacio", "Literal digito entero de 3 caracteres y un guión", "Literal digito entero de 4 caracteres"
+    #Esto nos da como resultado "(***) ***-****" Donde cada asterisco es un numero de l0 al 9 y los demás caracteres son exactamente como aparecen.
+    # La r antes de las comillas implica "raw", es decir, que el string pasa en crudo, esto evita que python lea escapes de linea erroneos.
     pattern = r"\(\d{3}\) \d{3}-\d{4}"
     return bool(re.match(pattern, phone))
 
